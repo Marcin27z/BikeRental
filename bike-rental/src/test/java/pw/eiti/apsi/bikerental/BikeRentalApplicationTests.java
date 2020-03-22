a02242612bc88969e55ebeebfef1a53bbe5c7c3b
ANadulny <adrian7991@wp.pl>
2020-03-22 13:41:27 +0100

    initial

---
+++ bike-rental/src/test/java/pw/eiti/apsi/bikerental/BikeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.eiti.apsi.bikerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
