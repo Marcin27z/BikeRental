93f463471573503575aefe28af3637f8bdb3e4b6
Tomasz Sałacki <salackitomasz@gmail.com>
2020-04-15 15:29:24 +0200

    Added method nod supported to custom error handler

--- src/main/java/BikeRental/BikeRentalREST/CustomErrorController.java
+++ src/main/java/BikeRental/BikeRentalREST/CustomErrorController.java
@@ -1,5 +1,6 @@
 package BikeRental.BikeRentalREST;
 
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,9 @@ public class CustomErrorController {
     @ExceptionHandler(NoHandlerFoundException.class)
     public CustomMessage handleNotFound(NoHandlerFoundException ex, HttpServletRequest request) {
         return new CustomMessage(404, ex.getMessage());
+    }
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public CustomMessage methodNotAllowed(HttpRequestMethodNotSupportedException ex, HttpServletRequest request){
+        return new CustomMessage(405, ex.getMessage());
     }
 }
