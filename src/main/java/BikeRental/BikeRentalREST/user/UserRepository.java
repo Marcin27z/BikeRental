2313ca74f2a11e9b2328675d4ec0b60cfa8858c7
mmodzel3 <apsi@DESKTOP.localdomain>
2020-04-20 13:19:24 +0200

    Fix - userExists query for email was not connected with isActive.

--- src/main/java/BikeRental/BikeRentalREST/user/UserRepository.java
+++ src/main/java/BikeRental/BikeRentalREST/user/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    @Query(value = "SELECT u FROM User u where u.email = ?1 or u.login = ?2 and u.isActive = true")
+    @Query(value = "SELECT u FROM User u where (u.email = ?1 or u.login = ?2) and u.isActive = true")
     Optional<User> userExists(String email, String login);
     Optional<User> findByEmailOrLoginOrPhoneNumber(String email, String login, String phoneNumber);
     Optional<User> findByEmail(String email);
