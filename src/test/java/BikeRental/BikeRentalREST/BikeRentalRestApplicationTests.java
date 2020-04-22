8240c1d0f2009ac194231ee7a26c144c19f45583
mmodzel3 <apsi@DESKTOP.localdomain>
2020-04-22 13:51:57 +0200

    Added getting token tests. Fixed building tests.

--- src/test/java/BikeRental/BikeRentalREST/BikeRentalRestApplicationTests.java
+++ src/test/java/BikeRental/BikeRentalREST/BikeRentalRestApplicationTests.java
@@ -2,11 +2,18 @@ package BikeRental.BikeRentalREST;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static com.jayway.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BikeRentalRestApplicationTests {
+
+    @LocalServerPort
+    int port;
 
 	@Test
 	void contextLoads() {
+		given().port(port).get("/").then().assertThat().extract();
 	}
 }
---
+++ src/test/java/BikeRental/BikeRentalREST/user/login/TokenControllerTests.java
@@ -0,0 +1,59 @@
+package BikeRental.BikeRentalREST.user.login;
+
+import BikeRental.BikeRentalREST.user.User;
+import BikeRental.BikeRentalREST.user.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static com.jayway.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TokenControllerTests {
+    private final static String FAKE_USERNAME = "fake";
+    private final static String FAKE_PASSWORD = "fake";
+
+    private final static String REAL_USERNAME = "real";
+    private final static String REAL_PASSWORD = "password";
+    private final static String REAL_EMAIL = "mail@mail.com";
+    private final static String REAL_PHONE_NUMBER = "55555555";
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void whenAskForTokenWithBadCredentialsThenGotErrorMessage() {
+        String response = given().port(port)
+                .param("username", FAKE_USERNAME)
+                .param("password", FAKE_PASSWORD)
+                .post("/token")
+                .then()
+                .statusCode(200)
+                .extract()
+                .asString();
+
+        assert (response.equals("no token found"));
+    }
+
+    @Test
+    void whenAskForTokenWithGoodCredentialsThenGotTokenAndNoErrorMessage() {
+        User user = new User(REAL_EMAIL, REAL_USERNAME, REAL_PASSWORD, REAL_PHONE_NUMBER);
+        userRepository.save(user);
+
+        String response = given().port(port)
+                .param("username", REAL_USERNAME)
+                .param("password", REAL_PASSWORD)
+                .post("/token")
+                .then()
+                .statusCode(200)
+                .extract()
+                .asString();
+
+        assert (!response.equals("no token found"));
+    }
+}
+
