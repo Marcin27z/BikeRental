d0913338926cb57eb73a1b84646e410dff15f893
mmodzel3 <45737456+mmodzel3@users.noreply.github.com>
2020-04-23 18:50:01 +0200

    Changed returning string token to LoginInfo object with admin flag.

--- src/main/java/BikeRental/BikeRentalREST/user/login/TokenController.java
+++ src/main/java/BikeRental/BikeRentalREST/user/login/TokenController.java
@@ -12,11 +12,7 @@ public class TokenController {
     private UserService userService;
 
     @PostMapping("/token")
-    public String getToken(@RequestParam("username") final String username, @RequestParam("password") final String password){
-        String token = userService.login(username, password);
-        if(StringUtils.isEmpty(token)){
-            return "no token found";
-        }
-        return token;
+    public LoginInfo getToken(@RequestParam("username") final String username, @RequestParam("password") final String password){
+        return userService.login(username, password);
     }
 }
