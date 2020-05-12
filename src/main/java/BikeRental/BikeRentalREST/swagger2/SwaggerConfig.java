2f78ce95881ad56fb410b03db5bf73ceb1cdac03
ANadulny <adrian7991@wp.pl>
2020-05-12 22:06:18 +0200

    added logger

--- src/main/java/BikeRental/BikeRentalREST/swagger2/SwaggerConfig.java
+++ src/main/java/BikeRental/BikeRentalREST/swagger2/SwaggerConfig.java
@@ -4,6 +4,7 @@ import static com.google.common.collect.Lists.newArrayList;
 
 import java.util.Collections;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,12 +19,14 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Slf4j
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+        log.info("Swagger api running");
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
