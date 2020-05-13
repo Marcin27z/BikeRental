d22cbc0991b3345eb0cf6945bbd2c50092e32c7d
Tomasz Sałacki <salackitomasz@gmail.com>
2020-05-14 00:22:14 +0200

    Removed redundant and unnecessay code

--- src/main/java/BikeRental/BikeRentalREST/rental/RentalController.java
+++ src/main/java/BikeRental/BikeRentalREST/rental/RentalController.java
@@ -37,64 +37,64 @@ public class RentalController {
     }
 
     @GetMapping("/api/rentals")
-    List<Rental> getRentalsAsUser(@RequestParam final Optional<Long> UserId, @RequestHeader(name="Authorization") String token){
-        if(UserId.isPresent() && UserId.get().equals(userService.findByToken(token).get().getUserId())){
-            return this.rentalService.getRentalsByUserId(UserId.get());
-        }else {
+    List<Rental> getRentalsAsUser(@RequestHeader(name="Authorization") String token){
+        if(!userService.findByToken(token).isPresent()){
             return new ArrayList<Rental>();
         }
+        Long UserId = userService.findByToken(token).get().getUserId();
+        return this.rentalService.getRentalsByUserId(UserId);
     }
 
     @PostMapping("/api/rentals")
-    CustomMessage makeRental(@RequestParam final Long userId, @RequestParam final Long stationId, @RequestHeader(name="Authorization") String token){
+    CustomMessage makeRental(@RequestParam final Long stationId, @RequestHeader(name="Authorization") String token){
+        if(!userService.findByToken(token).isPresent()){
+            return new CustomMessage(0, "User not found!");
+        }
+        Long userId = userService.findByToken(token).get().getUserId();
         Optional<User> user = userService.findById(userId);
         if(!user.isPresent()){
             return new CustomMessage(0, "User does not exist!");
         }
-        if(user.get().getUserId().equals(userService.findByToken(token).get().getUserId())){
-            Rental rental = new Rental();
-            Optional<Station> station = stationService.findById(stationId);
-            if(!station.isPresent()){
-                return new CustomMessage(0, "Station does not exist!");
-            }
-            List<Bike> bikesList = bikeService.getBikesByStationId(stationId);
-            if(bikesList.size() == 0){
-                return new CustomMessage(0, "No bikes are available!");
-            }
-            rental.setUser(user.get());
-            rental.setBike(bikesList.get(0));
-            bikeService.rentBike(bikesList.get(0).getBikeId());
-            rental.setRentalDate(LocalDateTime.now());
-            rentalService.addNewRental(rental);
-            return new CustomMessage(1, "Rental was made! Bike id = " + rental.getBike().getBikeId().toString());
-        }else{
-            return new CustomMessage(0, "Tried to rent for another user!");
+        Rental rental = new Rental();
+        Optional<Station> station = stationService.findById(stationId);
+        if(!station.isPresent()){
+            return new CustomMessage(0, "Station does not exist!");
         }
+        List<Bike> bikesList = bikeService.getBikesByStationId(stationId);
+        if(bikesList.size() == 0){
+            return new CustomMessage(0, "No bikes are available!");
+        }
+        rental.setUser(user.get());
+        rental.setBike(bikesList.get(0));
+        bikeService.rentBike(bikesList.get(0).getBikeId());
+        rental.setRentalDate(LocalDateTime.now());
+        rentalService.addNewRental(rental);
+        return new CustomMessage(1, "Rental was made! Bike id = " + rental.getBike().getBikeId().toString());
     }
 
     @PutMapping("/api/rentals")
-    CustomMessage endRental(@RequestParam final Long userId, @RequestParam final Long stationId, @RequestHeader(name="Authorization") String token){
-        if(userId.equals(userService.findByToken(token).get().getUserId())){
-            Optional<Rental> rental = this.rentalService.getOpenRentalByUserId(userId);
-            if(!rental.isPresent()){
-                return new CustomMessage(0, "no unfinished rentals!");
-            }
-            Optional<Station> station = this.stationService.findById(stationId);
-            if(!station.isPresent()){
-                return new CustomMessage(0, "station does not exist!");
-            }
-            if(this.bikeService.freeBike(rental.get().getBike().getBikeId()).isPresent()){
-                return new CustomMessage(0, "bike not found!");
-            }
-            this.bikeService.relocateBike(rental.get().getBike().getBikeId(), station.get());
+    CustomMessage endRental(@RequestParam final Long stationId, @RequestHeader(name="Authorization") String token){
+        if(!userService.findByToken(token).isPresent()){
+            return new CustomMessage(0, "User not found!");
+        }
+        Long userId = userService.findByToken(token).get().getUserId();
+        Optional<Rental> rental = this.rentalService.getOpenRentalByUserId(userId);
+        if(!rental.isPresent()){
+            return new CustomMessage(0, "no unfinished rentals!");
+        }
+        Optional<Station> station = this.stationService.findById(stationId);
+        if(!station.isPresent()){
+            return new CustomMessage(0, "station does not exist!");
+        }
+        if(this.bikeService.freeBike(rental.get().getBike().getBikeId()).isPresent()){
+            return new CustomMessage(0, "bike not found!");
+        }
+        this.bikeService.relocateBike(rental.get().getBike().getBikeId(), station.get());
 
-            if(rentalService.endRental(userId)){
-                return new CustomMessage(1, "Return successful!");
-            }else{
-                return new CustomMessage(0, "Return failed!");
-            }
+        if(rentalService.endRental(userId)){
+            return new CustomMessage(1, "Return successful!");
         }else{
-            return new CustomMessage(0, "Tried to end rental of another user!");
+            return new CustomMessage(0, "Return failed!");
         }
     }
 }
